1144d8b34abc6acf381207f5c77eeb45dd459bd4
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-11-24 20:34:36 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var app = require('./config/express.js')();
+const app = require('./config/express.js')();
 
-var porta = process.env.PORT || 3000;
+const porta = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
-app.listen(porta,function(){
-  console.log('Server running on port ' + porta);
-});
+app.listen(porta,() => console.log('Server running on port ' + porta));
