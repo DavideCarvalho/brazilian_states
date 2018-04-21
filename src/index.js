868452ad74c3228c3e9aae066b493ba452026419
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-04-21 12:43:26 -0300

    changing parcel build settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-api = require('./api/estados');
+const api = require('./api/estados');
 
 const npmPackage = {
   getCitiesFromState: api.getCities,
-  getStateFromCity: api.getStateFromCity
-}
+  getStateFromCity: api.getStateFromCity,
+};
 
 module.exports = npmPackage;
