86c4fd788c45c8c162fe18e1ca01d86a9786c63d
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-04-25 00:18:28 -0300

    added memoization on getStateCities. Added test case

--- test/state.test.js
+++ test/state.test.js
@@ -2,6 +2,9 @@ const api = require('../index');
 const spCities = require('../src/api/estados/saopaulo');
 const _ = require('lodash');
 const { expect } = require('chai');
+const _performance = require('perf_hooks').performance;
+const util = require('util');
+const debug = util.debuglog('performance');
 
 describe('state', () => {
   it('should return the given state from fullname', () => {
@@ -16,6 +19,20 @@ describe('state', () => {
     const cities = api.getStateCities({ state: 'sp' });
     expect(cities).to.deep.equal(spCities);
   });
+  it('second call to getStateCities function with the same parameters as the first call should be faster', () => {
+    _performance.mark('starting first run');
+    api.getStateCities({ state: 'sp' });
+    _performance.mark('ending first run');
+    _performance.mark('starting memoized run');
+    api.getStateCities({ state: 'sp' });
+    _performance.mark('ending memoized run');
+    _performance.measure('returning first run', 'starting first run', 'ending first run');
+    _performance.measure('returning memoized run', 'starting memoized run', 'ending memoized run');
+    const measurements = _performance.getEntriesByType('measure');
+    const times = [];
+    measurements.forEach(measurement => times.push(measurement.duration));
+    expect(times[1]).to.deep.below(times[0]);
+  });
   it('should return undefined if abbreviation state not found', () => {
     const cities = api.getStateCities({ state: 'someRandomState' });
     expect(cities).to.equal(undefined);
@@ -26,26 +43,24 @@ describe('state', () => {
   it('should throw an error if other parameter but state is passed to the function', () => {
     expect(() => api.getStateCities({ random: 'parameter' })).to.throw();
   });
-  it('should return json object of the state that has the given city if returnEntireJson is set to true', () => {
-    expect(api.getCityState({ city: 'Santos', returnEntireJson: true })).to.deep.equal(spCities);
+  it('should return json object of the state that has the given city if shouldReturnEntireJson is set to true', () => {
+    expect(api.getCityState({ city: 'Santos', shouldReturnEntireJson: true })).to.deep.equal(spCities);
   });
-  it('should return only the name of the state if returnEntireJson is not set', () => {
+  it('should return only the name of the state if shouldReturnEntireJson is not set', () => {
     expect(api.getCityState({ city: 'Santos' })).to.be.equal('São Paulo');
   });
-  it('should return only the name of the state if returnEntireJson is set to false', () => {
-    expect(api.getCityState({ city: 'Santos', returnEntireJson: false })).to.be.equal('São Paulo');
+  it('should return only the name of the state if shouldReturnEntireJson is set to false', () => {
+    expect(api.getCityState({ city: 'Santos', shouldReturnEntireJson: false })).to.be.equal('São Paulo');
   });
-  it('should return empty array if nothing is found and returnEntireJson is true', () => {
-    expect(
-      api.getCityState({ city: 'asldjoijsda', returnEntireJson: true }),
-    ).to.be.deep.equal({});
+  it('should return empty array if nothing is found and shouldReturnEntireJson is true', () => {
+    expect(api.getCityState({ city: 'asldjoijsda', shouldReturnEntireJson: true }))
+      .to.be.deep.equal({});
   });
-  it('should return empty string if nothing is found and returnEntireJson is false', () => {
-    expect(
-      api.getCityState({ city: 'asldjoijsda', returnEntireJson: false }),
-    ).to.be.deep.equal('');
+  it('should return empty string if nothing is found and shouldReturnEntireJson is false', () => {
+    expect(api.getCityState({ city: 'asldjoijsda', shouldReturnEntireJson: false }))
+      .to.be.deep.equal('');
   });
-  it('should return empty string if nothing is found and returnEntireJson is not set', () => {
+  it('should return empty string if nothing is found and shouldReturnEntireJson is not set', () => {
     expect(api.getCityState({ city: 'asldjoijsda' })).to.be.deep.equal('');
   });
 });
